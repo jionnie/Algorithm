3f9081c041b1a3628eb09884819c423dab3a17b8
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-10-23 09:59:23 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,17 +1,9 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        
         for(int i = 0; i < signs.length; i++) {
-            if (signs[i] == false) {
-                absolutes[i] *= -1;
-            }
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
         }
-        
-        for (int element : absolutes) {
-            answer += element;
-        }
-        
         return answer;
     }
 }
