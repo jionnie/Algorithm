f63a6c5f99155b667e754bfbad434dbb3c3d32b8
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-10-24 09:24:40 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -7,24 +7,15 @@ class Solution {
     public int solution(int[] numbers) {
         /*
         의사 코드
-        1. 존재 여부를 저장할 boolean 배열을 선언한다.
-        2. for 문으로 numbers의 요소를 돌면서 각 요소와 일치하는 isNumExists 인덱스 값을 true로 변경한다.
-        2. 존재하지 않는(false)인 요소의 인덱스를 int 변수 sum에 저장한다.
+        1. int sum을 0~9까지의 값을 전부 더한 값으로 초기화한다.
+        2. numbers 배열을 순회하면서 각 요소의 값을 sum에서 뺀다.
         */
-        int sum = 0;
-        boolean[] isNumExists = new boolean[10]; // boolean 기본 초기값은 false
+        int sum = 45; // 0~9까지의 합
         
-        for (int i = 0; i < numbers.length; i++) {
-            isNumExists[numbers[i]] = true;
-        }
-        
-        for (int i = 0; i < isNumExists.length; i++) {
-            if (isNumExists[i] == false) {
-                sum += i;
-            }
+        for (int num : numbers) {
+            sum -= num;
         }
         
         return sum;
-        
     }
 }
