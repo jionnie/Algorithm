80e8d96a9d39757699f7f1688b5519b6e9c2cd36
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-10-24 10:04:51 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -7,8 +7,8 @@ class Solution {
     public String solution(String s) {
         /*
         의사 코드
-        1. 문자열의 길이가 홀수면 substring으로 (문자열 길이/2 + 1) 위치의 문자열 추출
-        2. 문자열의 길이가 짝수면 substring으로(문자열/2) 인덱스 부터 다음 인덱스까지 문자열 추출
+        1. 문자열의 길이가 홀수면 substring으로 (문자열 길이/2) 위치의 문자열 추출
+        2. 문자열의 길이가 짝수면 substring으로 (문자열/2 - 1) 인덱스 부터 다음 인덱스까지 문자열 추출
         */
         int len = s.length();
         
