c3cdcd99001a617fd2ec52d077f5cc5ecdeae78a
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-10-25 22:13:57 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.10 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -8,22 +8,17 @@ class Solution {
     public int solution(int left, int right) {
         /**
         * 의사 코드
-        * 1. for 문을 left부터 right까지 반복
-        * 2. i를 이중 for 문으로 1부터 i까지 나누며(i / j가 0이면 약수) 약수 개수 세기
-        * 3. 약수의 개수가 짝수면 result에 더하고 홀수면 result에서 빼기
+        * 1. int i가 left~right 사이를 순회한다.
+        * 2. 만약 i가 제곱수면 약수의 개수는 홀수이다.(약수를 나열하면 가운데에 제곱근이 있다.)
+        * 3. i가 제곱수가 아니면 약수의 개수는 짝수이다.
         */
         int result = 0;
+        
         for (int i = left; i <= right; i++) {
-            int cnt = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) {
-                    cnt++;
-                }
-            }
-            if (cnt % 2 == 0) {
-                result += i;
-            } else {
+            if (i % Math.sqrt(i) == 0) {
                 result -= i;
+            } else {
+                result += i;
             }
         }
         
