211e81baca057ae902648611066c63a78eecf33e
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-10-28 14:16:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 2.98 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    /**
+    * 문제 해석
+    * 1. 매개값으로 주어진 n(n은 10진법)을 3진법 상에서 앞뒤로 뒤집는다.
+    * 2. 이를 다시 10진법으로 변환한 수를 리턴한다.
+    */
+    public int solution(int n) {
+        /**
+        * 문제 풀이
+        * 1. 10진법으로 주어진 n을 3으로 나누는데, 몫이 0이 될 때까지 나누면서 나머지를 char 배열에 저장한다.
+        * 2. 다시 10진법으로 변환한다.
+        */
+        String ternary = "";
+        int answer = 0;
+        
+        while (n / 3 != 0) {
+            ternary += String.valueOf(n % 3);
+            n /= 3;
+        }
+        
+        ternary += String.valueOf(n % 3);
+        
+        char[] reverse = ternary.toCharArray();
+        
+        for (int i = 0; i < reverse.length; i++) {
+            answer += (reverse[i] - 48) * (int) Math.pow(3, reverse.length - 1 - i);
+        }
+        
+        return answer;
+    }
+}
