1e7ebd009f8a6e8a190a3c350b8af4f84a29fb4f
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-10-28 11:26:33 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.06 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+    * 문제 해석
+    * 1. 입력 받은 두 수의 최대공약수와 최소공배수를 반환하는 메소드
+    * 2. 배열 형태로 반환
+    */
+    public int[] solution(int n, int m) {
+        /**
+        * 문제 풀이
+        * 1. 최대공약수(gcd): 유클리드 호제법
+        * 2. 최대공배수(lcm): 주어진 두 수를 곱한 값을 최대공약수로 나누기
+        */
+        int[] answer = new int[2];
+        int x = Math.max(n, m);
+        int y = Math.min(n, m);
+        
+        answer[0] = gcd(x, y);
+        answer[1] = (x * y) / answer[0];
+        
+        return answer;
+    }
+    
+    public static int gcd(int num1, int num2) {
+        return (num2 == 0) ? num1 : gcd(num2, num1 % num2);
+    }
+}
