982457add115da4709ff114ae913171047fb4383
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-11-07 11:08:39 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    /**
+    * 문제 해석
+    * 1. 배열을 i번째부터 j번째까지 자르고 정렬한다.
+    * 2. 그리고 나서 k번째에 있는 수를 구한다.
+    * 3. 배열 array와 [i, j, k]가 2차원 배열로 주어진다.
+    * 4. 각 연산 결과를 배열로 반환한다.
+    */
+    public int[] solution(int[] array, int[][] commands) {
+        /**
+        * 문제 풀이
+        * 1. commands 배열을 돌면서 i, j, k 값을 얻는다.
+        * 2. array를 i번째부터 j번째까지 얻고 정렬한다. (1부터 시작임)
+        * 3. k번째를 얻는다.
+        */
+        int[] answer = new int[commands.length];
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < commands.length; i++) {
+            for (int j = commands[i][0] - 1; j <= commands[i][0] - 1 + (commands[i][1] - commands[i][0]); j++) {
+                list.add(array[j]);
+            }
+            
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] - 1).intValue();
+            list.clear();
+        }
+        return answer;
+    }
+}
