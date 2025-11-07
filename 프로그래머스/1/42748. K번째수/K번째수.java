b815d72ba4ef52eba6c5132ba5f87a574db14249
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-11-07 11:28:41 +0900

    [level 1] Title: K번째수, Time: 1.24 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -16,17 +16,13 @@ class Solution {
         * 3. k번째를 얻는다.
         */
         int[] answer = new int[commands.length];
-        List<Integer> list = new ArrayList<>();
         
         for (int i = 0; i < commands.length; i++) {
-            for (int j = commands[i][0] - 1; j <= commands[i][0] - 1 + (commands[i][1] - commands[i][0]); j++) {
-                list.add(array[j]);
-            }
-            
-            Collections.sort(list);
-            answer[i] = list.get(commands[i][2] - 1).intValue();
-            list.clear();
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2] - 1];
         }
+        
         return answer;
     }
 }
