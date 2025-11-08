76aa383d9f2f09edb9bd4fade34b553f9c38c4c8
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-11-08 11:50:30 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.84 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    /**
+    * 문제 해석
+    * 1. 주어진 정수 배열 numbers에서 서로 다른 인덱스에 있는 2개의 수를 뽑는다.
+    * 2. 뽑은 "두 수를 더해 만들 수 있는 모든 수"를 배열에 "오름차순"으로 담아 리턴한다.
+    */
+    public int[] solution(int[] numbers) {
+        /**
+        * 문제 풀이
+        * 1. 이중 for 문을 돌면서 모든 경우의 수 탐색
+        * 2. HashSet으로 중복 저장 안 되도록 함
+        */
+        Set<Integer> set = new TreeSet<>();
+        
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        
+        int[] answer = new int[set.size()];
+        answer = set.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
+        
+        return answer;
+    }
+}
