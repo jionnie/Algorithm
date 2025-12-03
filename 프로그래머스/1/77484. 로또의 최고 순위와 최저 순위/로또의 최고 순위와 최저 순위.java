47f244109ef03c2fe43bd42a54b9be488fdbea8e
Jiwon Jung <134471035+jionnie@users.noreply.github.com>
2025-12-03 11:38:24 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.08 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
 * 문제 해석
 * 1. 로또에서 낙서가 칠해져 알아볼 수 없는 숫자를 0으로 표기한다.
@@ -12,49 +14,33 @@ class Solution {
         * 2. 각 배열을 비교해서 같은 값이 있을 때마다 일치하는 개수(correct) 카운트
         * 3. 1번에서 구한 수에서 0의 자리 수만큼 다 더하면 최고 순위, 더하지 않으면 최저 순위
         */
-        int[] answer = new int[2];
         int correct = 0;
         int zero = 0;
+        
+        // 당첨 번호를 Set으로 변환
+        Set<Integer> winSet = new HashSet<>();
+        
+        for (int num : win_nums) {
+            winSet.add(num);
+        }
+        
+        // 0 카운트 및 일치하는 개수 카운트
+        for (int num : lottos) {
+            if (num == 0) {
+                zero++;
+                continue;
+            }
             
-        for (int i = 0; i < lottos.length; i++) {
-            if (lottos[i] == 0) {
-                    zero++;
-                    continue;
-                }
-            
-            for (int j = 0; j < win_nums.length; j++) {
-                if (lottos[i] == win_nums[j]) {
-                    correct++;
-                }
+            if (winSet.contains(num)) {
+                correct++;
             }
         }
         
-        answer[0] = correct + zero;
-        answer[1] = correct;
+        int[] rank = { 6, 6, 5, 4, 3, 2, 1 };
         
-        for (int i = 0; i < 2; i++) {
-            switch (answer[i]) {
-                case 6:
-                    answer[i] = 1;
-                    break;
-                case 5:
-                    answer[i] = 2;
-                    break;
-                case 4:
-                    answer[i] = 3;
-                    break;
-                case 3:
-                    answer[i] = 4;
-                    break;
-                case 2:
-                    answer[i] = 5;
-                    break;
-                default:
-                    answer[i] = 6;
-                    break;
-            }
-        }
+        int best = correct + zero;
+        int worst = correct;
         
-        return answer;
+        return new int[] { rank[best], rank[worst] };
     }
 }
